6328453311f486afccdae36e6a0df0c2642b3537
youssef137ch <youssef.chourifi@polymtl.ca>
2020-09-14 08:43:38 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -9,7 +9,7 @@ def square_root(number: int) -> float:
 
 def square(number: int) -> int:
     # TODO completer la fonction
-    return math.pow(number, 2
+    return math.pow(number, 2)
 
 
 def main() -> None:
